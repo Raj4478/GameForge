78b299e9265653f049601fa4e620fe254764bb40
Rajeshwar Singh <132039556+Raj4478@users.noreply.github.com>
2024-11-20 17:16:39 +0000

    Pending changes exported from your codespace

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "Jsonwebtoken"
+import jwt from "jsonwebtoken"
 import {User} from "../models/user.model.js"
 
 
