dc4bcb449979db0c94379326b54007ca195358e7
Rajeshwar Singh <132039556+Raj4478@users.noreply.github.com>
2024-11-24 18:29:56 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ app.use(cors({
 //app.use(express.json())
 
 app.use(express.json({limit: "30kb"}))
-app.use(express.urlencoded({extended: true, limit: "30kb"}))
+app.use(express.urlencoded({extended: true, (limit: "30kb"}))
 //app.use(express.static("public"))
 app.use(cookieParser())
 
@@ -21,6 +21,8 @@ import userRouter from './routes/user.routes.js'
 
 
 
-
+app.get("/",(req,res)=>{
+    res.json("Hello");
+})
 app.use("/api/v1/users", userRouter)
 export {app}
