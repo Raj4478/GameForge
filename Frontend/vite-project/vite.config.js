2408d9b7f5947790423d0cec0a386aaed1974e76
Rajeshwar Singh <132039556+Raj4478@users.noreply.github.com>
2024-12-07 22:39:33 +0530

    Update vite.config.js

--- Frontend/vite-project/vite.config.js
+++ Frontend/vite-project/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://game-forge-nine.vercel.app/'
+      '/api':'https://game-forge-f.vercel.app/'
 
       }
       
