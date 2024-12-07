ac626c1eb6160feacf0ef44cfc63acfa6452e955
Rajeshwar Singh <132039556+Raj4478@users.noreply.github.com>
2024-12-07 22:32:25 +0530

    Update tailwind.config.js

--- Frontend/vite-project/tailwind.config.js
+++ Frontend/vite-project/tailwind.config.js
@@ -1,7 +1,7 @@
 //const withMT = require("@material-tailwind/react/utils/withMT");
 
- @type {import('tailwindcss').Config} 
-module.exports = withMT( {
+ //@type {import('tailwindcss').Config} 
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -35,4 +35,4 @@ module.exports = withMT( {
     },
   },
   plugins: [],
-})
+}
