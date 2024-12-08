261243ac953639e2638efb0bf34067671d702c63
Rajeshwar Singh <132039556+Raj4478@users.noreply.github.com>
2024-12-08 15:27:12 +0530

    Update vite.config.js

--- Frontend/vite-project/vite.config.js
+++ Frontend/vite-project/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'https://game-forge-kdny.vercel.app'
+  // server:{
+  //   proxy:{
+  //     '/api':'https://game-forge-kdny.vercel.app'
 
-      }
+  //     }
       
-    },
+  //   },
   plugins: [react()]
 })
